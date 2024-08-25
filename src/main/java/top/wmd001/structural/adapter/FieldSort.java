614b563757d12906daeebcb5eef241dd2b5686c9
wmd001 <habitdev@163.com>
2024-08-25 22:36:35 +0800

    适配器模式

---
+++ src/main/java/top/wmd001/structural/adapter/FieldSort.java
@@ -0,0 +1,24 @@
+package main.java.top.wmd001.structural.adapter;
+
+public class FieldSort {
+
+    private String field;
+    private String order;
+
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+}
