ca631e9fab88c1cf12e4304e4e87394653c83452
DayDreamer <habitdev@163.com>
2025-02-01 01:01:30 +0800

    add composite pattern code

---
+++ src/main/java/top/wmd001/structural/composite/Client.java
@@ -0,0 +1,20 @@
+package main.java.top.wmd001.structural.composite;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Leaf leaf1 = new Leaf("1");
+        Leaf leaf2 = new Leaf("2");
+
+        Composite composite = new Composite();
+        composite.add(leaf1);
+        composite.add(leaf2);
+
+        Composite root = new Composite();
+        root.add(composite);
+        root.add(new Leaf("3"));
+
+        root.operation();
+    }
+
+}
---
+++ src/main/java/top/wmd001/structural/composite/Component.java
@@ -0,0 +1,7 @@
+package main.java.top.wmd001.structural.composite;
+
+public interface Component {
+
+    void operation();
+
+}
