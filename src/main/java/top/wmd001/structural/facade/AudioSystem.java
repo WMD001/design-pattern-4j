5e4277cb66d3a3591de7327946936d723ad0a455
DayDreamer <habitdev@163.com>
2025-02-10 23:42:32 +0800

    add facade pattern code

---
+++ src/main/java/top/wmd001/structural/facade/AudioSystem.java
@@ -0,0 +1,15 @@
+package main.java.top.wmd001.structural.facade;
+
+public class AudioSystem {
+    public void turnOn() {
+        System.out.println("Audio system is turned on.");
+    }
+
+    public void turnOff() {
+        System.out.println("Audio system is turned off.");
+    }
+
+    public void setVolume(int volume) {
+        System.out.println("Audio system volume set to " + volume + ".");
+    }
+}
---
+++ src/main/java/top/wmd001/structural/facade/VideoSystem.java
@@ -0,0 +1,15 @@
+package main.java.top.wmd001.structural.facade;
+
+public class VideoSystem {
+public void turnOn() {
+    System.out.println("Video system is turned on.");
+}
+
+public void turnOff() {
+    System.out.println("Video system is turned off.");
+}
+
+public void playVideo(String video) {
+    System.out.println("Video system is playing: " + video + ".");
+}
+}
